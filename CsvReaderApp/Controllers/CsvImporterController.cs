1955bd3d2668eaada4c0b3d80f7919ffe5cd3599
Aisha Hersi <24008663@STUDENT.BMET.AC.UK>
2026-02-05 11:54:36 +0000

    version 1

---
+++ CsvReaderApp/Controllers/CsvImporterController.cs
@@ -0,0 +1,52 @@
+﻿using CsvReaderApp.Services;
+using CsvReaderApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsvReaderApp.Controllers
+{
+    [Route("[controller]")]
+    public class CsvImporterController : Controller
+    {
+        private readonly CsvService _csvService;
+        public CsvImporterController(CsvService csvService)
+        {
+            _csvService = csvService;
+        }
+        [HttpGet("")]
+        [HttpGet("Index")]
+        public IActionResult Index()
+        {
+            return View(new List<User>());
+        }
+        [HttpPost("")]
+        [HttpPost("Index")]
+        public IActionResult Index(IFormFile csvFile)
+        {
+            if (csvFile != null && csvFile.Length > 0)
+            {
+                using (var stream = csvFile.OpenReadStream())
+                {
+                    try
+                    {
+                        var users = _csvService.ReadCsvFile(stream).ToList();
+                        return View(users);
+                    }
+                    catch (ApplicationException ex)
+                    {
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError(string.Empty, $"An unexpected error occurred: {ex.Message}");
+                    }
+                }
+
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Please select a valid CSV file.");
+            }
+            return View(new List<User>());
+        }
+    }
+}
---
+++ CsvReaderApp/Services/CsvService.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper;
+using CsvHelper.TypeConversion;
+using CsvReaderApp.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+namespace CsvReaderApp.Services
+{
+    public class CsvService
+    {
+        public IEnumerable<User> ReadCsvFile(Stream fileStream)
+        {
+            try
+            {
+                using (var reader = new StreamReader (fileStream))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<User>();
+                    return records.ToList();
+                }
+            }
+
+            catch (HeaderValidationException ex)
+            {
+                throw new ApplicationException("CSV file is invald.", ex);
+            }
+            catch (TypeConverterException ex)
+            {
+                throw new ApplicationException("CSV file contains invalid data format.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error reading CSV file", ex);
+            }
+        }
+    }
+}
